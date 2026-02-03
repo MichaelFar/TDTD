3c72b0e62d2f85c9aa8fc8e7b1505bac58d02522
MichaelFar <rarrafarrar@gmail.com>
2026-02-03 14:24:50 -0800

    Created touch input class that can drag from ui onto the tower slots

---
+++ Assets/Scripts/PlayerInputContainer.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using UnityEngine.InputSystem.EnhancedTouch;
+public class PlayerInputContainer : MonoBehaviour
+{
+
+    public GameObject testTowerPrefab;
+
+    public Touchscreen touchscreen;
+
+    private GameObject selectedTowerObject;
+
+    private enum InputState {PLACEMENTSCREEN};//Handles instances where certain input contexts should be checked, IE we shouldn't place towers on the pause menu
+
+    private InputState currentInputState = InputState.PLACEMENTSCREEN;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnEnable()
+    {
+        TouchSimulation.Enable();
+        EnhancedTouchSupport.Enable();
+        //UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += get_touch_details;
+    }
+    void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        ProcessTouchInput();
+        
+        
+    }
+
+    void ProcessTouchInput()
+    {
+        foreach (var touch in Touch.activeTouches)
+        {
+            RaycastHit hit;
+
+            Ray ray_vector = Camera.main.ScreenPointToRay(touch.screenPosition);
+
+            bool ray = Physics.Raycast(ray_vector, out hit, Mathf.Infinity);//Physics.Raycast(Camera.main.ScreenToWorldPoint(touch.screenPosition),Camera.main.transform.forward,out hit,1000.0f);//Camera.main.ScreenPointToRay(touch.screenPosition);
+            if (ray)
+            {
+                if (hit.collider.GetComponent<TowerSlot>())
+                {
+                    TowerSlot this_tower_slot = hit.collider.GetComponent<TowerSlot>();
+                    if (touch.phase == TouchPhase.Ended)
+                    {
+                        if(selectedTowerObject != null)
+                        {
+                            this_tower_slot.InstanceNewTower(selectedTowerObject);
+                            selectedTowerObject = null;
+                        }
+                        
+                            
+                    }
+                }
+                print(hit.collider.gameObject.name);
+                Debug.Log($"{touch.touchId}: {hit.point},{touch.phase}");
+            }
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                selectedTowerObject = null;
+            }
+            
+
+        }
+        
+    }
+
+    public void SetCurrentTowerObject(GameObject new_tower_object)
+    {
+        print("Receiving tower " + new_tower_object);
+        selectedTowerObject = new_tower_object;
+    }
+}
---
+++ Assets/Scripts/TowerSlot.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+
+public class TowerSlot : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    bool isOccupied = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstanceNewTower(GameObject new_tower_object)
+    {
+        if(!isOccupied)
+        {
+            
+            Instantiate(new_tower_object, transform.position, Quaternion.identity);
+            isOccupied = true;
+        }
+            
+    }
+}
