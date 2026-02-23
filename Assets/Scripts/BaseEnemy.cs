d90193ee671c426633e2ad006f20a1f4d09f9b65
MichaelFar <rarrafarrar@gmail.com>
2026-02-23 13:38:26 -0800

    Created a functional demolish state with a button to toggle it, added ui for coin collection

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -16,6 +16,10 @@ public class BaseEnemy : MonoBehaviour
     public Action<float> Reached_End_Of_Path;
 
     public float damagePowerToCore = 1.0f;
+
+    public GameObject[] potentialDropsArray;
+
+    public float chanceToDrop = 25.0f;
     void Start()
     {
         splineAnimate.Completed += BehaviorOnCompletedPath;
@@ -28,12 +32,14 @@ public class BaseEnemy : MonoBehaviour
     }
     public void DestroyThisEnemy()
     {
+        CheckThenCreateDrop();
         Destroy(gameObject);
     }
 
     public void BehaviorOnCompletedPath()
     {
         Reached_End_Of_Path.Invoke(damagePowerToCore);
+        
         Destroy(gameObject);
     }
 
@@ -42,4 +48,13 @@ public class BaseEnemy : MonoBehaviour
         splineAnimate.Play();
     }
     
+    private void CheckThenCreateDrop()
+    {
+        float roll = UnityEngine.Random.Range(1.0f, 100.0f);
+        if(roll <= chanceToDrop)
+        {
+            int rand_index = UnityEngine.Random.Range(0, potentialDropsArray.Length);
+            Instantiate(potentialDropsArray[rand_index], transform.position, Quaternion.identity);
+        }
+    }
 }
