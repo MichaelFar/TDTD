77161e99110cbffa498c0a1afcef9aea11add0a7
MichaelFar <rarrafarrar@gmail.com>
2026-04-09 14:52:43 -0700

    Added support for random enemies

--- Assets/Scripts/WaveScriptableObject/Wave.cs
+++ Assets/Scripts/WaveScriptableObject/Wave.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Wave", menuName = "Scriptable Objects/Wave")]
+[CreateAssetMenu(fileName = "Wave", menuName = "Waves/Wave")]
 public class Wave : ScriptableObject
 {
+    
     public GameObject[] enemiesInWave;
+
+    
+    
     public float[] intervalForThisEnemy;
     public float defaultSpawnInterval = 3.0f;
 }
--- Assets/Scripts/WaveSpawnHandler.cs
+++ Assets/Scripts/WaveSpawnHandler.cs
@@ -82,8 +82,19 @@ public class WaveSpawnHandler : MonoBehaviour
         if (deltaTracker >= spawnInterval && numInstancedEnemies < numEnemiesToSpawn)
         {
             numInstancedEnemies += 1;
-
-            enemyTracker.SpawnEnemy(enemiesThisWave[currentEnemyIndex], splinePath, currentcamera, core);
+            if (enemiesThisWave[currentEnemyIndex].GetComponent<RandomEnemy>())
+            {
+                
+                GameObject enemy_instance = Instantiate(enemiesThisWave[currentEnemyIndex], transform.position, Quaternion.identity);
+                RandomEnemy random_enemy = enemy_instance.GetComponent<RandomEnemy>();
+                //print("Chosen random enemy is " + random_enemy.chosenRandomEnemy);
+                enemyTracker.SpawnEnemy(random_enemy.ChooseRandomEnemyAndDestroySelf(), splinePath, currentcamera, core);
+            }
+            else
+            {
+                enemyTracker.SpawnEnemy(enemiesThisWave[currentEnemyIndex], splinePath, currentcamera, core);
+            }
+                
             UpdateSpawnInterval();
             currentEnemyIndex += 1;
             deltaTracker = 0.0f;
@@ -138,6 +149,7 @@ public class WaveSpawnHandler : MonoBehaviour
     }
     private void UpdateSpawnInterval()
     {
+        //Check if there is an interval set for the current enemy, if not, use default spawn interval set in the wave object
         spawnInterval = wavesArray[currentWaveIndex].intervalForThisEnemy.Length - 1 > currentEnemyIndex ? wavesArray[currentWaveIndex].intervalForThisEnemy[currentEnemyIndex] : wavesArray[currentWaveIndex].defaultSpawnInterval;
     }
     public void StopWave()
